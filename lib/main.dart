5d22f019f0e4dcc996c68d0fcb6e499db5557a3b
mylanewby <167246119+mylanewby@users.noreply.github.com>
2025-02-22 22:16:47 -0500

    Correctly displays the equation

--- lib/main.dart
+++ lib/main.dart
@@ -44,62 +44,97 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  // Individual parts of the display aka the operands (nums) and operators
+  // Can be null just to check if previous parts of the equation need to be updated
   String? _firstNum;
   String? _secondNum;
   String? _operator;
-  String? _equation;
-  int _result = 0;
+  // The actual display
+  String _equation = "";
+  // The numerical result
+  // Can be null to check if the display can be updated
+  int? _result;
 
+  // Creates the number operands, allowing the user to create multi-digit operands
   _setNumber(String num) {
-    setState( () {
-      // Able to add numbers to _firstNum if the operator isn't set
-      if (_operator != null) {
-        _secondNum != null ? _secondNum = '$_secondNum$num' : _secondNum = num;
-      } else {
-        _firstNum != null ? _firstNum = '$_firstNum$num' : _firstNum = num;
+    setState(() {
+      // Ensures the user presses the clear button before entering another equation
+      if (_result == null) {
+        // Only adds _secondNum numbers if the _firstNum isn't set
+        if (_operator != null) {
+          _secondNum != null ? _secondNum = '$_secondNum$num' : _secondNum = num;
+        } else {
+          _firstNum != null ? _firstNum = '$_firstNum$num' : _firstNum = num;
+        }
+        // Updates the display
+        _setEquation();
       }
     });
   }
-
+  // Sets the operand
   _setOperator(String operator) {
     setState(() {
-      _operator = operator;
-      //_setEquation();
-    });
-  }
-
-  /*_setEquation() {
-    setState(() {
-      _equation = '$_firstNum $_operator $_secondNum';
-    });
-  }*/
-
-  _solveEquation() {
-    setState(() {
-      if (_firstNum != null && _operator != null && _secondNum != null) {
-        int first = int.tryParse(_firstNum!) ?? 0;
-        int second = int.tryParse(_secondNum!) ?? 0;
-        if (_operator! == '+') {
-          _result = first + second;
-        } else if (_operator! == '-') {
-          _result = first - second;
-        } else if (_operator! == '*') {
-          _result = first * second;
-        } else if (_operator! == '/' && second !=0) {
-          _result = first ~/ second;
-        }
-      } else {
-        _result = 1;
+      // Ensures the user presses the clear button before entering another equation
+      if (_result == null) {
+        _operator = operator;
+        // Updates the display
+        _setEquation();
       }
     });
   }
 
+  // Calculates the result of the operands
+  _solveEquation() {
+    setState(() {
+      // Ensures no null numbers are involved in calculations
+      // tryParse() returns null for invalid inputs, which will become 0 here
+      int first = int.tryParse(_firstNum!) ?? 0;
+      int second = int.tryParse(_secondNum!) ?? 0;
+      // Ensures each calculation is handled
+      if (_operator! == '+') {
+        _result = first + second;
+      } else if (_operator! == '-') {
+        _result = first - second;
+      } else if (_operator! == '*') {
+        _result = first * second;
+      // Prevents division by 0 error
+      } else if (_operator! == '/' && second !=0) {
+        _result = first ~/ second;
+      }
+      // Displays the result to the screen
+      _equation = _result.toString();
+    });
+  }
+
+  // Sets the _equation operand, which displays to the screen
+  _setEquation() {
+    setState(() {
+      // Prevents displaying "null" and displays nothing instead
+      String first = _firstNum ?? '';
+      String second = _secondNum ?? '';
+      String operator = _operator ?? '';
+
+      // Handles displaying each equation part separately
+      if (_secondNum != null) {
+        _equation = '$first $operator $second';
+      } else if (_operator != null) {
+        _equation = '$first $operator';
+      } else if (_firstNum != null) {
+        _equation = first;
+      } else {
+        _equation = "";
+      }
+    });
+  }
+
+  // Sets nullable variables to null and the display to nothing
   void _clearButton() {
     setState(() {
       _firstNum = null;
       _secondNum = null;
       _operator = null;
-      _result = 0;
+      _result = null;
+      _equation = "";
     });
   }
 
@@ -114,18 +149,11 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            // Displays the current equation
             Text(
-              _firstNum ?? ''
+              _equation
             ),
-            Text(
-              _operator ?? ''
-            ),
-            Text(
-              _secondNum ?? ''
-            ),
-            Text(
-              _result.toString()
-            ),
+            // 7 8 9 / Row
             Row( 
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
